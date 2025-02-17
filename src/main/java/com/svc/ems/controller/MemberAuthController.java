73acad73998948a29b01033eb0cc97acca56ee84
Sean <o0987570061>
2025-02-17 14:53:57 +0800

    調整url跟帳號密碼 sean 0217

--- src/main/java/com/svc/ems/controller/MemberAuthController.java
+++ src/main/java/com/svc/ems/controller/MemberAuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth/member")
+@RequestMapping("/api/auth/member")
 public class MemberAuthController {
 
 
