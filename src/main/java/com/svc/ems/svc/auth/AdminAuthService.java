393406f8015f71a24f8fc9675c2e5113ee7571db
Sean <o3245236@gmail.com>
2025-03-25 09:18:53 +0800

    tiny push

--- src/main/java/com/svc/ems/svc/auth/AdminAuthService.java
+++ src/main/java/com/svc/ems/svc/auth/AdminAuthService.java
@@ -8,5 +8,24 @@ public interface AdminAuthService {
 
 
 
-    public ResponseEntity<ApiResponseTemplate<?>> userRegister(AdminRegisterRequest req);
+    public ResponseEntity<ApiResponseTemplate<?>> adminRegister(AdminRegisterRequest req);
+
+    public ResponseEntity<ApiResponseTemplate<?>> adminLogin(AdminRegisterRequest req);
+
+    public ResponseEntity<ApiResponseTemplate<?>> adminLogout();
+
+    public ResponseEntity<ApiResponseTemplate<?>> adminRefreshToken(String refreshToken);
+
+    public ResponseEntity<ApiResponseTemplate<?>> adminUpdateProfile(AdminRegisterRequest req);
+
+
+    public ResponseEntity<ApiResponseTemplate<?>> adminGetMemberList(AdminRegisterRequest req);
+
+    public ResponseEntity<ApiResponseTemplate<?>> adminGetMemberProfile(AdminRegisterRequest req);
+
+    public ResponseEntity<ApiResponseTemplate<?>> adminUpdateMemberProfile(AdminRegisterRequest req);
+
+    public ResponseEntity<ApiResponseTemplate<?>> adminDeleteMemberProfile(AdminRegisterRequest req);
+
+    public ResponseEntity<ApiResponseTemplate<?>> adminUpdatePwd(AdminRegisterRequest req);
 }
