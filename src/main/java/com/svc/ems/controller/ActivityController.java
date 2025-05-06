800ffcef2779e4f0848303e70e27bd8c96d3a71e
Omar <omar10218@gmail.com>
2025-05-06 14:59:49 +0800

    20250506 改成get by Curtis

--- src/main/java/com/svc/ems/controller/ActivityController.java
+++ src/main/java/com/svc/ems/controller/ActivityController.java
@@ -6,7 +6,7 @@ import com.svc.ems.svc.activity.ActivityService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +22,7 @@ public class ActivityController {
 
     private final ActivityService activityService;
 
-    @PostMapping("/eventQuery")
+    @GetMapping("/eventQuery")
     @Operation(summary = "活動清單")
     public ResponseEntity<ApiResponseTemplate<List<ActivityQueryResponse>>> getActivityList() {
         return activityService.activityQuery();
